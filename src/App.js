0fb7783504d3aff88fc1b0c1f096d91eeca17a29
kapil <kapilchhipa4@gmail.com>
2020-09-21 16:20:49 +0530

    new project of movies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { Component } from "react";
+import Movies from "./components/movies";
+import "./App.css";
 
-class App extends React.Component{
-
-
-    render(){
-        return (
-        <div>
-            <h1> kapil</h1>
-        </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Movies />
+      </main>
+    );
+  }
 }
 
-export default App
+export default App;
