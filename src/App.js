fa7c61a2863df31a3c6299aed1bf1ab17c8f9a97
kapil <kapilchhipa4@gmail.com>
2020-09-22 12:03:56 +0530

    counter -app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import Movies from "./components/movies";
 import "./App.css";
-
+import Navbar from './components/navbar'
+import Counters from './components/counters'
 class App extends Component {
   render() {
     return (
+     <div>
+        <Navbar/>
       <main className="container">
-        <Movies />
+        <Counters />
       </main>
+      </div>
     );
   }
 }
